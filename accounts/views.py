9022d20900e154db51ae2841302520ed7a127025
abhirup-deb <debabhirup1999@gmail.com>
2019-04-12 23:13:24 +0530

    Fourth commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def Signup(request):
-    return render(request,'accounts/Signup.html')
+    if request.method == 'POST':
+        if request.POST['Pass'] == request.POST['ConfPass']:
+            try:
+                user = User.objects.get(Username=request.POST['Username'])
+                return render(request, 'accounts/Signup.html', {'error': 'Username has already been taken!'})
+            except User.DoesNotExist:
+                user=User.objects.create_user(request.POST['Username'],Password=request.POST['Pass'],Email=request.POST['Email'])
+                auth.login(request,user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/Signup.html', {'error': 'Passwords must match!'})
+    else:
+        return render(request,'accounts/Signup.html')
+    
 
 def Login(request):
     return render(request,'accounts/Login.html')
